36ecd4a2b0eae420ec2d72610531017600bff382
jinosh05 <jinoshprabhuraj5@gmail.com>
2022-10-15 22:19:45 +0530

    Regenerated route file

--- lib/routes/routes.gr.dart
+++ lib/routes/routes.gr.dart
@@ -1,77 +1,128 @@
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
 // AutoRouteGenerator
 // **************************************************************************
+//
+// ignore_for_file: type=lint
 
-import 'package:auto_route/auto_route.dart' as _i1;
-import 'package:flutter/material.dart' as _i2;
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:auto_route/auto_route.dart' as _i4;
+import 'package:flutter/material.dart' as _i5;
 
-import '../main.dart' as _i3;
-import '../second_page.dart' as _i4;
-import '../third_page.dart' as _i5;
+import '../main.dart' as _i1;
+import '../second_page.dart' as _i2;
+import '../third_page.dart' as _i3;
 
-class Router extends _i1.RootStackRouter {
-  Router([_i2.GlobalKey<_i2.NavigatorState>? navigatorKey])
+class Router extends _i4.RootStackRouter {
+  Router([_i5.GlobalKey<_i5.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i1.PageFactory> pagesMap = {
-    MyAppRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+  final Map<String, _i4.PageFactory> pagesMap = {
+    MyAppRoute.name: (routeData) {
+      return _i4.MaterialPageX<dynamic>(
         routeData: routeData,
-        builder: (_) {
-          return _i3.MyApp();
-        }),
-    SecondPageRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        child: _i1.MyApp(),
+      );
+    },
+    SecondPageRoute.name: (routeData) {
+      return _i4.MaterialPageX<dynamic>(
         routeData: routeData,
-        builder: (_) {
-          return _i4.SecondPage();
-        },
-        fullscreenDialog: true),
-    ThirdPageRoute.name: (routeData) => _i1.CustomPage<dynamic>(
+        child: _i2.SecondPage(),
+        fullscreenDialog: true,
+      );
+    },
+    ThirdPageRoute.name: (routeData) {
+      final args = routeData.argsAs<ThirdPageRouteArgs>();
+      return _i4.CustomPage<dynamic>(
         routeData: routeData,
-        builder: (data) {
-          final args = data.argsAs<ThirdPageRouteArgs>();
-          return _i5.ThirdPage(key: args.key, text: args.text);
-        },
-        transitionsBuilder: _i1.TransitionsBuilders.slideLeftWithFade,
+        child: _i3.ThirdPage(
+          key: args.key,
+          text: args.text,
+        ),
+        transitionsBuilder: _i4.TransitionsBuilders.slideLeftWithFade,
         opaque: true,
-        barrierDismissible: false)
+        barrierDismissible: false,
+      );
+    },
   };
 
   @override
-  List<_i1.RouteConfig> get routes => [
-        _i1.RouteConfig(MyAppRoute.name, path: '/'),
-        _i1.RouteConfig(SecondPageRoute.name, path: '/second-page'),
-        _i1.RouteConfig(ThirdPageRoute.name, path: '/third-page')
+  List<_i4.RouteConfig> get routes => [
+        _i4.RouteConfig(
+          MyAppRoute.name,
+          path: '/',
+        ),
+        _i4.RouteConfig(
+          SecondPageRoute.name,
+          path: '/second-page',
+        ),
+        _i4.RouteConfig(
+          ThirdPageRoute.name,
+          path: '/third-page',
+        ),
       ];
 }
 
-class MyAppRoute extends _i1.PageRouteInfo {
-  const MyAppRoute() : super(name, path: '/');
+/// generated route for
+/// [_i1.MyApp]
+class MyAppRoute extends _i4.PageRouteInfo<void> {
+  const MyAppRoute()
+      : super(
+          MyAppRoute.name,
+          path: '/',
+        );
 
   static const String name = 'MyAppRoute';
 }
 
-class SecondPageRoute extends _i1.PageRouteInfo {
-  const SecondPageRoute() : super(name, path: '/second-page');
+/// generated route for
+/// [_i2.SecondPage]
+class SecondPageRoute extends _i4.PageRouteInfo<void> {
+  const SecondPageRoute()
+      : super(
+          SecondPageRoute.name,
+          path: '/second-page',
+        );
 
   static const String name = 'SecondPageRoute';
 }
 
-class ThirdPageRoute extends _i1.PageRouteInfo<ThirdPageRouteArgs> {
-  ThirdPageRoute({_i2.Key? key, required String text})
-      : super(name,
-            path: '/third-page',
-            args: ThirdPageRouteArgs(key: key, text: text));
+/// generated route for
+/// [_i3.ThirdPage]
+class ThirdPageRoute extends _i4.PageRouteInfo<ThirdPageRouteArgs> {
+  ThirdPageRoute({
+    _i5.Key? key,
+    required String text,
+  }) : super(
+          ThirdPageRoute.name,
+          path: '/third-page',
+          args: ThirdPageRouteArgs(
+            key: key,
+            text: text,
+          ),
+        );
 
   static const String name = 'ThirdPageRoute';
 }
 
 class ThirdPageRouteArgs {
-  const ThirdPageRouteArgs({this.key, required this.text});
+  const ThirdPageRouteArgs({
+    this.key,
+    required this.text,
+  });
 
-  final _i2.Key? key;
+  final _i5.Key? key;
 
   final String text;
+
+  @override
+  String toString() {
+    return 'ThirdPageRouteArgs{key: $key, text: $text}';
+  }
 }
